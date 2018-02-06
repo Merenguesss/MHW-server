df330010f4d9ddcf83e80481b4c06a0f61927b26
Jia Shi <v_shijia@baidu.com>
2018-02-06 10:59:11 +0800

    update

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
   res.render('error');
+  return res.status(err.status || 500);
 });
 
 app.listen(3000);
